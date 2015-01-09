71a603f25a85e2be67536c8e3fe686a479944c2e
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2015-01-09 13:42:50 +0000

    fixup! arm: support SI (simple var assignment)

--- c4.c
+++ c4.c
@@ -563,7 +563,7 @@ int *codegenarm(int *jitmem, int reloc)
       *je++ = 0xe5900000;       // ldr r0, [r0]
     else if (i == SI) {
       *je++ = 0xe49d1004;       // pop {r1}
-      *je++ = 0xe5810000;       // ldr r0, [r1]
+      *je++ = 0xe5810000;       // str r0, [r1]
     }
     else if (i == PSH)
       *je++ = 0xe52d0004;       // push {r0}
